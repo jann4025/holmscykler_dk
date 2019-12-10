e89c0c3f351c74b399d8cb278649cab3fd81ce83
Jannick Holm <jannick@wecode.dk>
2019-12-10 10:03:25 +0100

    Vue test

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import ComingSoon from '../views/ComingSoon'
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "comingsoon",
+    component: ComingSoon
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
