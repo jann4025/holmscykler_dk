196b9ec38e94961053ebf7f759230b801b39fd37
Jannick Holm <jannick@wecode.dk>
2019-12-17 22:38:47 +0100

    Responsive work on landingpage

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,30 @@ document.addEventListener('DOMContentLoaded', start);
 function start() {
   getHeaderHeight();
   document.addEventListener('scroll', readScroll);
+  document.querySelector('#toggle input').addEventListener('click', e => {
+    e.target.checked
+      ? document
+          .querySelector('.mobile .navigation_links')
+          .classList.add('showMenu')
+      : document
+          .querySelector('.mobile .navigation_links')
+          .classList.remove('showMenu');
+  });
 }
 
 function readScroll() {
-  let scrollPosY = window.pageYOffset;
-  const header = document.querySelector('header');
-  console.log(scrollPosY);
-
-  scrollPosY >= 171
-    ? header.classList.add('bg-black')
-    : header.classList.remove('bg-black');
+  const root = document.documentElement;
+  const navigation = document.querySelector('nav');
+  const navigationRect = navigation.getBoundingClientRect();
+  if (navigationRect.height === 0) {
+  } else {
+    console.log(true);
+    let scrollPosY = window.pageYOffset;
+    const header = document.querySelector('header');
+    scrollPosY >= 171
+      ? header.classList.add('bg-black')
+      : header.classList.remove('bg-black');
+  }
 }
 
 function getHeaderHeight() {
